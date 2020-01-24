a980dd64264621c8773fbc7f72fa6363f3e4152c
Raphael Schmidt <rs91288@gmail.com>
2020-01-24 13:56:49 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter} from 'react-router-dom';
+import Find_cocktail from "./Component/Find_Cocktail/Find_cocktail"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Find_cocktail/>
+      </BrowserRouter>
   );
 }
 
---
+++ src/Component/Find_Cocktail/Find_cocktail.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react"
+
+class Find_cocktail extends Component {
+    render() {
+        return (
+            <div>
+                <h1>RubbeldieKatz</h1>
+            </div>
+        )
+    }
+}
+
+export default Find_cocktail;
